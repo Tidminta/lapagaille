a5cc7d354dd3b905b659c5b0ab5b61cdf8f9e8f0
McFlyy <mcflyy@MacBook-Air-de-McFlyy.local>
2020-01-09 19:53:52 +0100

    ft_strtrim.c

--- libftest_/Libftest/my_config.sh
+++ libftest_/Libftest/my_config.sh
@@ -5,7 +5,7 @@
 ###################################
 
 #Edit the next line with the path of your libft project.
-PATH_LIBFT=~/42/WM/libftest_/libft
+PATH_LIBFT="/Users/mcflyy/42/WM/libftest_/libft"
 
 #If you use a relative path, don't put double quotes.
 #For example :
