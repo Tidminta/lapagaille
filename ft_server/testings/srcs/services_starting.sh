b496cfbe86ec994e041b9d41a91b038322b34400
McFlyy <mcflyy@MacBook-Air-de-McFlyy.local>
2020-05-25 20:10:52 +0200

    nettoyage ft_server + add cub3d

--- ft_server/testings/srcs/services_starting.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-service nginx start
-service mysql start
-service php7.3-fpm start
-mysql -e "CREATE DATABASE IF NOT EXISTS wordpress; CREATE USER IF NOT EXISTS wordpress_user@localhost IDENTIFIED BY 'password'; GRANT ALL PRIVILEGES ON wordpress.* TO wordpress_user@localhost; FLUSH PRIVILEGES;"
