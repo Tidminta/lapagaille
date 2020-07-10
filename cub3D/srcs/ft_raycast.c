5a1576925fc8630b24e9112b061efb9b3b4df95b
McFlyy <mcflyy@MacBook-Air-de-McFlyy.local>
2020-07-10 20:43:49 +0200

    raycast v2

--- cub3D/srcs/ft_raycast.c
+++ cub3D/srcs/ft_raycast.c
@@ -6,7 +6,7 @@
 /*   By: tidminta <tidminta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 19:10:15 by tidminta          #+#    #+#             */
-/*   Updated: 2020/07/07 18:56:26 by tidminta         ###   ########.fr       */
+/*   Updated: 2020/07/10 20:41:56 by tidminta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,102 +25,126 @@
 
 int				ft_raycast(t_mapinfos **map_tmp, t_mlx	**mlx)
 {
+	int 		cpt;
 	int			x;
 	int			y;
-	double		tern_tmp;
 	char		**map2d;
 	t_mapinfos	*map;
-	t_player	*player;
+	t_player	*p;
 
 	map = *map_tmp;
 	map2d = map->map_tab;
 	x = 0;
 	y = 0;
-	player = ft_playerinit();
-	if (!player)
+	cpt = 0;
+	p = ft_playerinit();
+	if (!p)
 		return (-1);
 	map->mlx = *mlx;
-	player->posx = map->start_x;
-	player->posy = map->start_y;
-	player->dirx = -1;
-	player->diry = 0;
-	player->planx = 0;
-	player->plany = 0.66;
-	player->x = 0;
-	while ((size_t)player->x < map->resolution->res_x)
+	p->posx = map->start_x;
+	p->posy = map->start_y;
+	p->dirx = -1;
+	p->plany = 0.66;
+	while (p->x < (int)map->resolution->res_x)
 	{
-		//calcul de la position du vecteur et sa direction
-		player->camx = (2 * player->x / ((double)map->resolution->res_x - 1));
-		player->raydx = player->dirx + player->planx * player->camx;
-		player->raydy = player->diry + player->plany * player->camx;
-		//length of ray from one x or y-side to next x or y-side
-		player->mapx = (int)player->posx;
-		player->mapy = (int)player->posy;
-		player->deltadx = fabs(1 / player->raydx);
-		player->deltady = fabs(1 / player->raydy);
-		// Alternative code for deltaDist in case division through zero is not supported
-		tern_tmp = (player->raydx == 0) ? 1 : fabs(1 / player->raydx);
-		player->deltadx = (player->raydy == 0) ? 0 : tern_tmp;
-		tern_tmp = (player->raydy == 0) ? 1 : fabs(1 / player->raydy);
-		player->deltady = (player->raydx == 0) ? 1 : tern_tmp;
-		//calculate step and initial sideDist
-		if (player->raydx < 0)
+		p->hit = 0;
+		p->camx = 2 * p->x / (double)map->resolution->res_x - 1;
+		p->raydx = p->dirx + p->planx * p->camx;
+		p->raydy = p->diry + p->plany * p->camx;
+		p->mapx = (int)p->posx;
+		p->mapy = (int)p->posy;
+		p->deltadx = fabs(1 / p->raydx);
+		p->deltady = fabs(1 / p->raydy);
+		p->deltadx = (p->raydy == 0) ? 0 : ((p->raydx == 0) ? 1 : fabs(1 / p->raydx));
+		p->deltady = (p->raydx == 0) ? 0 : ((p->raydy == 0) ? 1 : fabs(1 / p->raydy));
+		if (p->raydx < 0)
 		{
-			player->stepx = -1;
-			player->sidedx = (player->posx - player->mapx) * player->deltadx;
+			p->stepx = -1;
+			p->sidedx = (p->posx - p->mapx) * p->deltadx;
 		}
 		else
 		{
-			player->stepx = 1;
-			player->sidedx = (player->mapx + 1.0 - player->posx) * player->deltadx;
+			p->stepx = 1;
+			p->sidedx = (p->mapx + 1.0 - p->posx) * p->deltadx;
 		}
-		if (player->raydy < 0)
+		if (p->raydy < 0)
 		{
-			player->stepy = -1;
-			player->sidedy = (player->posy - player->mapy) * player->deltady;
+			p->stepy = -1;
+			p->sidedy = (p->posy - p->mapy) * p->deltady;
 		}
 		else
 		{
-			player->stepy = 1;
-			player->sidedy = (player->mapy + 1.0 - player->posy) * player->deltady;
+			p->stepy = 1;
+			p->sidedy = (p->mapy + 1.0 - p->posy) * p->deltady;
 		}
-		while (!player->hit)
+		while (p->hit == 0)
 		{
-			if (player->sidedx < player->sidedy)
+			if (p->sidedx < p->sidedy)
 			{
-				player->sidedx += player->deltadx;
-				player->mapx += player->stepx;
-				player->side = 0;
+				p->sidedx += p->deltadx;
+				p->mapx += p->stepx;
+				p->side = 0;
 			}
 			else
 			{
-				player->sidedy += player->deltady;
-				player->mapy += player->stepy;
-				player->side = 1;
+				p->sidedy += p->deltady;
+				p->mapy += p->stepy;
+				p->side = 1;
 			}
-			if (map2d[player->mapy][player->mapx] == '1')
+			if (map2d[p->mapx][p->mapy] == '1')
 			{
-				printf("[mapy = %d][mapx = %d]\n", player->mapy, player->mapx);
-				player->hit = 1;
+				p->hit = 1;
 			}
+			cpt++;
 		}
-		if (player->side == 0)
-			player->perpwd = (player->mapx - player->posx + (1 - player->stepx) / 2) / player->raydx;
+		if (p->side == 0)
+			p->perpwd = (p->mapx - p->posx + (1 - p->stepx) / 2) / p->raydx;
 		else
-			player->perpwd = (player->mapy - player->posy + (1 - player->stepy) / 2) / player->raydy;
-		player->lineheight = ((int)map->win_h / player->perpwd);
-		player->drawstart = -player->lineheight / 2 + map->win_h / 2;
-		player->drawstart = (player->drawstart < 0) ? 0 : player->drawstart;
-		player->drawend = player->lineheight / 2 + map->win_h / 2;
-		if (player->drawend >= (int)map->win_h)
-			player->drawend = map->win_h - 1;
-		// player->drawend = (player->drawend >= (int)map->win_h) ? (map->win_h - 1): player->drawend;
-		// ft_print_playerinfos(player);
-		player->x += 1;
-		// player->mapx += player->stepx;
-		// player->mapy += player->stepy;
+			p->perpwd = (p->mapy - p->posy + (1 - p->stepy) / 2) / p->raydy;
+		p->lineheight = (int)(map->resolution->res_y / p->perpwd);
+		p->drawstart = -p->lineheight / 2 + map->resolution->res_y / 2;
+		if (p->drawstart < 0)
+			p->drawstart = 0;
+		p->drawend = p->lineheight / 2 + map->resolution->res_y / 2;
+		if (p->drawend >= (int)map->resolution->res_y)
+			p->drawend = map->resolution->res_y - 1;
+		ft_print_playerinfos(p);
+/*
+*************************************
+**			  DRAWING              **
+*************************************
+*/
+	size_t y;
+	int *tab;
+
+	y = 0;
+	tab = map->mlx->img->data;
+	// while (y < p->drawstart)
+	// {
+	// 	tab[ y * map->resolution->res_x * p->x] = ft_create_trgb(0, 0, 0, 0);
+	// }
+	y = p->drawstart;
+	while((int)y < p->drawend - 1)
+	{
+		tab[ y * map->resolution->res_x * p->x] = 0x808080;
+		y++;
+		printf("print wall\n");
 	}
-	mlx_loop(map->mlx->mlx_ptr);
+	while (y < map->resolution->res_y - 1)
+	{
+		tab[ y * map->resolution->res_x * p->x] = 0x77b5fe;
+		y++;
+		printf("print ceil\n");
+	}
+	
+/*
+*************************************
+**			  END             		**
+*************************************
+*/
+		p->x += 1;
+	}
 	mlx_put_image_to_window(map->mlx->mlx_ptr, map->mlx->win, map->mlx->img->img_ptr, 0, 0);
+	mlx_loop(map->mlx->mlx_ptr);
 	return (1);
 }
