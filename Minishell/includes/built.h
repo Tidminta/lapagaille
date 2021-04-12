a4ac5d3ea6ca5b541f6155cefbfd80f5981bed1f
Tidiane MINTA <tidminta@e3r10p2.42.fr>
2021-04-12 18:26:08 +0200

    18:25 12/04

--- Minishell/includes/built.h
+++ Minishell/includes/built.h
@@ -6,25 +6,31 @@
 /*   By: tidminta <tidminta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 15:47:40 by tidminta          #+#    #+#             */
-/*   Updated: 2021/04/10 18:17:25 by tidminta         ###   ########.fr       */
+/*   Updated: 2021/04/12 15:26:11 by tidminta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILT_H
 # define BUILT_H
 
-int			ft_my_unset(t_msh *msh, t_list *lst);
+typedef struct	s_ret
+{
+	char	*s;
+	int		i;
+}				t_ret;
 
-int			ft_pop_node(t_env_lair *env_lair, char **unset_tab);
+int				ft_my_unset(t_msh *msh, t_list *lst);
 
-int			ft_my_export(t_msh *msh, t_list *element);
+int				ft_pop_node(t_env_lair *env_lair, char **unset_tab);
 
-int			ft_my_env(t_env_lair *lst);
+int				ft_my_export(t_msh *msh, t_list *element);
 
-int			ft_my_pwd(void);
+int				ft_my_env(t_env_lair *lst);
 
-int			ft_my_cd(t_msh *msh, t_list *lst);
+int				ft_my_pwd(void);
 
-int			ft_my_echo(t_msh *msh, t_list *lst);
+int				ft_my_cd(t_msh *msh, t_list *lst);
+
+int				ft_my_echo(t_msh *msh, t_list *lst);
 
 #endif
--- Minishell/srcs/builtins/my_echo.c
+++ Minishell/srcs/builtins/my_echo.c
@@ -6,57 +6,67 @@
 /*   By: tidminta <tidminta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/12 06:06:53 by loamar            #+#    #+#             */
-/*   Updated: 2021/04/10 18:24:04 by tidminta         ###   ########.fr       */
+/*   Updated: 2021/04/12 18:21:56 by tidminta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/libshell.h"
 
-static int		check_if_exist(t_msh *msh, char *str, int indice)
+static t_ret		*ft_check(t_msh *msh, t_env_list *lst, char *str, int infos)
 {
 	char		*s;
-	t_env_list	*lst;
+	t_ret		*ret;
 	size_t		i;
+	size_t		len;
 
+	if (!msh || !str || !lst)
+		return (NULL);
 	i = 0;
-	lst = msh->env_lair->start;
-	while (indice-- >= 0)
+	ret = (t_ret*)malloc(sizeof(t_ret));
+	ret->i = 0;
+	ret->s = 0;
+	while (infos-- >= 0)
 		str++;
-	while (str[i] && (str[i] != 32 && str[i] != 34
+	while ((str[i]) && (str[i] != 32 && str[i] != 34
 		&& str[i] != 36 && str[i] != 39))
 		i++;
 	if (i && (!(s = ft_substr(str, 0, (size_t)i))))
-		return (ERROR);
+		return (NULL);
 	while (lst != NULL)
 	{
 		if ((ft_strncmp(lst->content, "(null)", 6) != 0)
 			&& (ft_strncmp(s, lst->content, i) == 0))
 		{
-			if (s)
-				free(s);
-			s = ft_strchr(lst->content, 61) + 1;
-			ft_putstr_fd(s, 1);
-			return (SUCCESS);
+			free(s);
+			ret->i = i;
+			// ret->s = ft_strdup(ft_strchr(lst->content, 61) + 1);
+			ret->s = ft_strchr(lst->content, 61) + 1;
+			return (ret);
 		}
 		lst = lst->next;
 	}
 	if (s)
 		free(s);
-	return (ERROR);
+	return (NULL);
 }
 
-static int	ft_handle_elem(t_msh *msh, char *str, int quote)
+// dont forget  int redirection in handle processsing
+//if (redirection) stocker dans char* sinn putchar_fd
+// recap l79 && modifier ft_check
+static int	ft_handle(t_msh *msh, char *str, size_t size, int quote)
 {
-	int		i;
-	int		indic;
-	size_t	len;
+	int			i;
+	int			indic;
+	t_ret		*ret;
+	t_env_list	*lst;
 
+	// printf("[DEBUT HANDLE][STR = %s][len = %zu]\n", str, ft_strlen(str));
 	if (!msh || !str)
 		return (ERROR);
 	i = (!quote) ? -1 : 0;
 	indic = 0;
-	len = ft_strlen(str);
-	if (i >= 0 && str[i] == SQUOTE)
+	lst = msh->env_lair->start;
+	if (i == 0 && str[i] == SQUOTE)
 	{
 		while (str[++i + 1])
 			ft_putchar_fd(str[i], 1);
@@ -64,10 +74,23 @@ static int	ft_handle_elem(t_msh *msh, char *str, int quote)
 	}
 	while (str[++i])
 	{
-		if (str[i] && (str[i] == 36 && (check_if_exist(msh, str, i) >= 0)))
+		if ((str[i] == 36) && (ret = ft_check(msh, lst, str, i)))
+		{
+			i += ret->i - 1;
+			ft_putstr_fd(ret->s, 1);
+			// free_ret(ret->s);
+			// ret->i = NULL;
+			free(ret);
 			indic = 1;
-		else if (indic != 1)
+		}
+		else if (indic != 1 && (i != (ft_strlen(str) - 1) && str[i] != 34))
 			ft_putchar_fd(str[i], 1);
+		else if ((size_t)i < ft_strlen(str) && (i != (ft_strlen(str) - 1) && str[i] != 34))
+		{
+			// indic = 0;
+			// printf("[str[%i]][%c]\n", i, str[i]);
+			ft_putchar_fd(str[i], 1);
+		}
 	}
 	return (SUCCESS);
 }
@@ -87,7 +110,7 @@ int			ft_my_echo(t_msh *msh, t_list *lst)
 	{
 		quote = (element->token == ARGS
 		&& (element->content[0] == 34 || element->content[0] == 39)) ? 1 : 0;
-		ft_handle_elem(msh, element->content, quote);
+		ft_handle(msh, element->content, ft_strlen(element->content), quote);
 		ft_putchar_fd(32, 1);
 		element = element->next;
 	}
