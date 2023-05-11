e9bed4cb8aac9d5d76750013f2d044a6b19b79ec
Tidiane Minta <tidiane@Tidianes-MacBook-Air.local>
2023-05-11 16:17:56 -0400

    pipex 11 mai

--- Minishell/pipex/includes.h
+++ Minishell/pipex/includes.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   includes.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tminta <tminta@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tidiane <tidiane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 19:19:27 by tminta            #+#    #+#             */
-/*   Updated: 2023/03/16 19:52:41 by tminta           ###   ########.fr       */
+/*   Updated: 2023/05/10 01:05:37 by tidiane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,70 @@
 
 # include "../libft/libft.h"
 # include <fcntl.h>
+# include <unistd.h>
+# include <sys/wait.h>
+
+// typedef	struct	s_path
+// {
+// 	char	*content;
+// 	t_path	*n;
+// }				t_path;
+
+
+// TOKEN : 1 = cmd | 2 = args
 
 typedef struct s_pipe
 {
-	char	**envp;
-	char	*file1;
-	char	*cmd1;
-	char	*file2;
-	char	*cmd2;
-	char	*path;
-	int		pid;
-	int		errno;
-	int		fd1;
-	int		fd2;
-	int		pipe[2];
+	char			**envp;
+	char			*file1;
+	char			**cmd;
+	char			*cmd1;
+	char			**o_cmd1;
+	char			**only_cmd;
+	char			*file2;
+	char			*cmd2;
+	char			*o_cmd2;
+	char			**path;
+	char			**paths;
+	char 			**args1;
+	char 			**args2;
+	char			***f_args;
+	int				pid;
+	int				err_no;
+	int				fd1;
+	int				fd2;
+	int				pipe[2];
 }				t_pipe;
+
+
+/* ******************************************** */
+//PARSING 
 
 int		processing(t_pipe *box);
 
 t_pipe	*ft_init(char **argv, char **envp);
 
-void	ft_open(t_pipe *box);
+void	ft_parsing_step(t_pipe **box, char **argv, char **envp);
+
+char	*ft_only_cmd(t_pipe *box, int indic);
+
+t_pipe	*ft_init(char **argv, char **envp);
+
+void	ft_free_split2(t_pipe *box);
+
+char	*split_path(char **envp);
+
+int		ft_get_path(t_pipe	*box, int indic);
+
+void	ft_parsing_step(t_pipe **box, char **argv, char **envp);
+
+
+/* ******************************************** */
+//EXEC
+void	ft_exec(t_pipe	*box, int indic);
+
+int		processing(t_pipe *box);
+
+// void	ft_open(t_pipe *box);
 
 #endif
---
+++ Minishell/pipex/parse.c
@@ -0,0 +1,144 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tidiane <tidiane@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/04 14:26:30 by tidiane           #+#    #+#             */
+/*   Updated: 2023/05/10 01:09:54 by tidiane          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes.h"
+
+void    ft_get_cmd(t_pipe **box)
+{
+    (*box)->only_cmd[0] = ft_only_cmd(*box, 0);
+    (*box)->only_cmd[1] = ft_only_cmd(*box, 1);
+}
+
+void	ft_parsing_step(t_pipe **box, char **argv, char **envp)
+{
+    int indic;
+
+    indic = -1;
+	*box = ft_init(argv, envp);
+    if (!(*box))
+        return ;
+	ft_get_cmd(box);
+	// ft_get_arg(); NOT NECESSARY
+    while (++indic < 2)
+        ft_get_path(*box, indic);
+}
+
+char	*ft_only_cmd(t_pipe *box, int indic)
+{
+	char	*str;
+	char	**table;
+	size_t	i;
+
+	table = (char **)malloc(sizeof(char*) * 3);
+	// table[0] = box->cmd1;
+	// table[1] = box->cmd2;
+    table[0] = box->cmd[0];
+	table[2] = box->cmd[1];
+	i = 0;
+	while (table[indic][i] && table[indic][i] != ' ')
+		i++;
+	str = ft_substr(table[indic], 0, i);
+    free(table[0]);
+    free(table[1]);
+    free(table);
+	return (str);
+}
+
+t_pipe	*ft_init(char **argv, char **envp)
+{
+	t_pipe	*tools;
+
+
+	tools = (t_pipe *)malloc(sizeof(t_pipe));
+	if (!tools)
+		return (NULL);
+	tools->envp = envp;
+	tools->file1 = ft_strdup(argv[1]);
+	tools->cmd1 = ft_strdup(argv[2]);
+    tools->cmd[0] = ft_strdup(argv[2]);
+    tools->cmd[1] = ft_strdup(argv[3]);
+	tools->o_cmd1 = 0;
+    tools->only_cmd = (char**)malloc(sizeof(char*) * 3);
+    tools->only_cmd[2] = NULL;
+	tools->file2 = ft_strdup(argv[4]);
+	tools->cmd2 = ft_strdup(argv[3]);
+	tools->o_cmd2 = 0;
+	tools->path = 0;
+	tools->pid = 0;
+	tools->err_no = 0;
+	tools->fd1 = 0;
+	tools->fd2 = 0;
+	tools->paths = 0;
+    tools->f_args = (char***)malloc(sizeof(char**) * 3);
+    tools->f_args[2] = NULL;
+	tools->f_args[0] = ft_split(argv[2], ' ');
+	tools->f_args[1] = ft_split(argv[3], ' ');
+
+	return (tools);
+}
+
+void	ft_free_split2(t_pipe *box)
+{
+	int i;
+
+	i = -1;
+	while (box->paths[i])
+		free(box->paths[i]);
+}
+
+char	*split_path(char **envp)
+{
+	int	i;
+	char	*path;
+
+	i = 0;
+	while (ft_strncmp("PATH", envp[i], ft_strlen("PATH")))
+		i++;
+	path = ft_substr(envp[i], 5, (size_t)ft_strlen(envp[i]));
+    ft_strtrim(path, "\n");
+	return (path);
+}
+
+int	ft_get_path(t_pipe	*box, int indic)
+{
+	char	*tmp;
+	// char	*path;
+	int		res;
+	int		i;
+
+	i = -1;
+	res = 0;
+	tmp = split_path(box->envp);
+	box->paths = ft_split(tmp, ':');
+	if (tmp)
+		free(tmp);
+	while (box->paths[++i])
+	{
+		tmp = ft_strdup(box->paths[i]);
+		tmp = ft_strjoin(tmp, "/");
+		tmp = ft_strjoin(tmp, box->only_cmd[indic]);
+		ft_strtrim(tmp, "\n");
+        // printf("[complet path][%s]\n", tmp);
+        // sleep(1);
+		if (access(tmp, F_OK) == 0)
+		{
+			box->path[indic] = ft_strdup(tmp);
+            // printf("[final path][%s]\n", box->path);
+			res = 1;
+            // sleep(1);
+		}
+		free(tmp);
+	}
+	// if (box->paths)
+	// 	ft_free_split2(box);
+	return (res);
+}
