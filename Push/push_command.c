985b766efe52828211aeaab7865449accd288fe6
Tidiane Minta <tidiane@Tidianes-MacBook-Air.local>
2023-10-09 16:29:19 -0400

    push swapv1

---
+++ Push/push_command.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_command.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tidiane <tidiane@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 13:28:03 by tidiane           #+#    #+#             */
+/*   Updated: 2023/10/09 16:25:42 by tidiane          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/*
+** be carfull if !stack 
+*/
+static void	push(t_stack_node **dest, t_stack_node **src)
+{
+	t_stack_node	*node_to_push;
+
+	if (NULL == *src)
+		return ;
+	node_to_push = *src;
+	*src = (*src)->next;
+	if (*src)
+		(*src)->prev = NULL;
+	node_to_push->prev = NULL;
+	if (NULL == *dest)
+	{
+		*dest = node_to_push;
+		node_to_push->next = NULL;
+	}
+	else
+	{
+		node_to_push->next = *dest;
+		node_to_push->next->prev = node_to_push;
+		*dest = node_to_push;
+	}
+}
+
+void	pa(t_stack_node **a, t_stack_node **b, bool checker)
+{
+	push(a, b);
+	if (!checker)
+		write(1, "pa\n", 3);
+}
+
+void	pb(t_stack_node **b, t_stack_node **a, bool checker)
+{
+	push(b, a);
+	if (!checker)
+		write(1, "pb\n", 3);
+}
---
+++ Push/tiny_sort.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tiny_sort.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tidiane <tidiane@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 13:27:28 by tidiane           #+#    #+#             */
+/*   Updated: 2023/10/09 13:31:57 by tidiane          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+
+bool	stack_sorted(t_stack_node *stack)
+{
+	if (NULL == stack)
+		return (1);
+	while (stack->next)
+	{
+		if (stack->value > stack->next->value)
+			return (false);
+		stack = stack->next;
+	}
+	return (true);
+}
+
+static t_stack_node	*find_highest(t_stack_node *stack)
+{
+	int				highest;
+	t_stack_node	*highest_node;
+
+	if (NULL == stack)
+		return (NULL);
+	highest = INT_MIN;
+	while (stack)
+	{
+		if (stack->value > highest)
+		{
+			highest = stack->value;
+			highest_node = stack;
+		}
+		stack = stack->next;
+	}
+	return (highest_node);
+}
+
+/*
+3 elements sorting
+*/
+void	tiny_sort(t_stack_node **a)
+{
+	t_stack_node	*highest_node;
+
+	highest_node = find_highest(*a);
+	if (*a == highest_node)
+		ra(a, false);
+	else if ((*a)->next == highest_node)
+		rra(a, false);
+	if ((*a)->value > (*a)->next->value)
+		sa(a, false);
+}
+
+/*
+**	5 elements sorting
+*/
+void	handle_five(t_stack_node **a, t_stack_node **b)
+{
+	while (stack_len(*a) > 3)
+	{
+		init_nodes(*a, *b);
+		finish_rotation(a, find_smallest(*a), 'a');
+		pb(b, a, false);
+	}
+}
